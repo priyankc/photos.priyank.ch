b5b09aeb9f353ddd1f7a7e7e0844e39a4330a837
Priyank Chodisetti <priyank.ch@gmail.com>
2025-01-30 22:06:20 -0800

    Fix lint issues

--- src/app/components/selects.tsx
+++ src/app/components/selects.tsx
@@ -1,10 +1,10 @@
 "use client"
-import {useState, useEffect} from "react";
+import {useState, useEffect, ReactElement} from "react";
 import {fetchPics} from "@/app/util/pics";
 import Image from "next/image";
 
 export function Selects() {
-    const [imgList, setImgList] = useState<any[]>([]);
+    const [imgList, setImgList] = useState<ReactElement[]>([]);
 
     useEffect(() => {
         const pics = async () => {
@@ -13,7 +13,7 @@ export function Selects() {
                 console.error(error);
             } else {
 
-                const newImgList = [];
+                const newImgList: ReactElement[] = [];
                 data?.forEach((item, index) =>{
                     newImgList.push(<Image
                         src={process.env.NEXT_PUBLIC_SUPABASE_URL + '/storage/v1/object/public/Selects/' + item.name}
--- src/app/util/pics.tsx
+++ src/app/util/pics.tsx
@@ -4,7 +4,8 @@ import {createClient} from "@supabase/supabase-js";
 
 export async function fetchPics() {
     "use server"
-    const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
+    const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL || '',
+        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '');
     return await supabase.storage
         .from('Selects')
         .list()

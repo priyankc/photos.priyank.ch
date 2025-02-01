04dd9556e1e9ca1d3e0911c60a5e192b13559804
Priyank Chodisetti <priyank.ch@gmail.com>
2025-01-31 20:35:36 -0800

    Bugfix: Use correct supabase service role key name

--- auth.tsx
+++ auth.tsx
@@ -4,7 +4,7 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL || '',
-    process.env.NEXT_PUBLIC_SERVICE_ROLE_KEY || ''
+    process.env.SUPABASE_SERVICE_ROLE_KEY || ''
 );
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
